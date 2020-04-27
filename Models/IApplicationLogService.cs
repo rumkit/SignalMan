b29b7062fd6f3bd65e1cb2d33811144cdeb9580a
Anton Mitsengendler <a.mic@rtc.ru>
2020-04-27 13:01:38 +0300

    Connect checks for isConnected
    introduces isConnected flag
    changed log window to textBox

--- Models/IApplicationLogService.cs
+++ Models/IApplicationLogService.cs
@@ -5,5 +5,7 @@ namespace SignalMan.Models
     public interface IApplicationLogService
     {
         event EventHandler<string> MessageReceived;
+
+        void PublishMessage(string message);
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using ReactiveUI;
 using SignalMan.Models;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace SignalMan.ViewModels
@@ -9,32 +12,45 @@ namespace SignalMan.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private readonly IHubConnector _connector;
-        private IApplicationLogService _logService;
+        private readonly IApplicationLogService _logService;
 
         private string _hubUrl;
-
         public string HubUrl
         {
             get => _hubUrl;
             set => this.RaiseAndSetIfChanged(ref _hubUrl, value);
         }
 
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set => this.RaiseAndSetIfChanged(ref _isConnected, value);
+        }
+
+        private string _logMessages = string.Empty;
+        public string LogMessages
+        {
+            get => _logMessages;
+            set => this.RaiseAndSetIfChanged(ref _logMessages, value);
+        }
+
         public ReactiveCommand<Unit, Unit> Connect { get; }
         public ReactiveCommand<Unit, Unit> Disconnect { get; }
-        public ObservableCollection<string> LogMessages { get; }
+
+        
 
         public MainWindowViewModel(IHubConnector connector, IApplicationLogService logService)
         {
             _connector = connector;
             _logService = logService;
 
-            LogMessages = new ObservableCollection<string>();
-            logService.MessageReceived += (sender, message) => LogMessages.Add(message);
-
+            logService.MessageReceived += (sender, message) => LogMessages += $"{message}{Environment.NewLine}";
+            
             var canConnect = this.WhenAnyValue(
-                x => x.HubUrl,
-                (hubUrl) => 
-                    !string.IsNullOrEmpty(hubUrl));
+                x => x.HubUrl, x => x.IsConnected,
+                (hubUrl, isConnected) => 
+                    !string.IsNullOrEmpty(hubUrl) && !isConnected);
 
             Connect = ReactiveCommand.CreateFromTask(async () => await ConnectAsync(),
                 canConnect);
@@ -45,12 +61,22 @@ namespace SignalMan.ViewModels
         private async Task<Unit> DisconnectAsync()
         {
             await _connector.DisconnectAsync();
+            IsConnected = false;
             return Unit.Default;
         }
 
         private async Task<Unit> ConnectAsync()
         {
-            await _connector.ConnectAsync(HubUrl);
+            IsConnected = true;
+            try
+            {
+                await _connector.ConnectAsync(HubUrl);
+            }
+            catch
+            {
+                _logService.PublishMessage($"Connection to {HubUrl} failed");
+                IsConnected = false;
+            }
             return Unit.Default;
         }
     }
